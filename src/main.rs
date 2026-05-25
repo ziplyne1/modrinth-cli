b9320c2901df2e85fbc4585fc80af6c4732f630e
Pax Willoughby <kilter_waspish0l@icloud.com>
2026-05-25 00:57:04 -0700

    Implement basic web requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    mod_name: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args: Cli = Cli::parse();
+
+    let url: String = format!(
+        "https://api.modrinth.com/v2/project/{}/version",
+        args.mod_name
+    );
+    let response: reqwest::Response = reqwest::get(&url).await.unwrap();
+    let body: String = response.text().await.unwrap();
+
+    println!("Fetching: {url}");
+    println!("{body}");
 }
