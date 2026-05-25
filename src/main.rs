3187b607d757768ffd04ebf9cb3ad6b909d71820
Pax Willoughby <kilter_waspish0l@icloud.com>
2026-05-25 01:19:14 -0700

    Add limit to number of versions printed

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,36 @@ struct Cli {
 async fn main() {
     let args: Cli = Cli::parse();
 
-    let url: String = format!(
-        "https://api.modrinth.com/v2/project/{}/version",
-        args.mod_name
-    );
-    let response: reqwest::Response = reqwest::get(&url).await.unwrap();
-    let body: Vec<Version> = response.json::<Vec<Version>>().await.unwrap();
+    let body: Vec<Version> = get_mod_versions(&args.mod_name).await;
 
-    println!("ID: {}", body[0].id);
-    println!("Name: {}", body[0].name);
-    println!("Version Number: {}", body[0].version_number);
-    println!("Version Type: {}", body[0].version_type);
-    println!("Loaders: {:?}", body[0].loaders);
-    println!("Game Versions: {:?}", body[0].game_versions);
+    println!("{} versions available.", body.len());
+    println!("Print how many?");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let num_versions: usize = input.trim().parse().unwrap_or(0);
+
+    for i in 0..num_versions {
+        print_version_info(&body[i]);
+    }
+}
+
+async fn get_mod_versions(mod_name: &str) -> Vec<Version> {
+    let url: String = format!("https://api.modrinth.com/v2/project/{}/version", mod_name);
+    let response: reqwest::Response = reqwest::get(&url).await.unwrap();
+
+    return response.json::<Vec<Version>>().await.unwrap();
+}
+
+fn print_version_info(version: &Version) {
+    println!("-----------------------------");
+    println!("ID: {}", version.id);
+    println!("Name: {}", version.name);
+    println!("Version Number: {}", version.version_number);
+    println!("Version Type: {}", version.version_type);
+    println!("Loaders: {:?}", version.loaders);
+    println!("Game Versions: {:?}", version.game_versions);
     println!("Files:");
-    for file in &body[0].files {
+    for file in &version.files {
         println!("  - URL: {}", file.url);
         println!("    Filename: {}", file.filename);
         println!("    Primary: {}", file.primary);
